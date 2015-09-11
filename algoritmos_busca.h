3e78bafb8a347d71950423108fb4b05054b80e3d
Yuri Thomas <yuri.tpinheiro@gmail.com>
2015-09-11 17:17:40 -0300

    Fim da implementação da busca em profundidade

--- algoritmos_busca.h
+++ algoritmos_busca.h
@@ -10,27 +10,41 @@
 
 /* Busca em profundidade */
 
-void busca_profundidade(Celula *partida, Celula *chegada)
+void definir_rota(Celula *celula);
+void busca_profundidade(Celula *partida, Celula *chegada, double peso_v, double peso_h, double peso_d);
+
+void definir_rota(Celula *celula)
 {
-    if (partida == chegada){
+    if (celula->get_p_pai() != celula)
+    {
+        celula->set_pai(celula->get_p_pai());
+        definir_rota(celula->get_pai());
+    }
+}
+
+void busca_profundidade(Celula *partida, Celula *chegada, double peso_v, double peso_h, double peso_d)
+{
+    if (partida == chegada)
+    {
         if (partida->get_g() < partida->get_h())
-             definir_rota(partida);
+        {
+            partida->set_h(partida->get_g());
+            definir_rota(partida);
+        }
         // Definir melhor rota
 
     } else {
-        for (int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++)
+        {
             if (partida->get_vizinho(i) != nullptr
                     && partida->get_vizinho(i)->get_p_pai() == nullptr
-                    && partida->get_vizinho(i)->get_tipo() != OBSTACULO){
+                    && partida->get_vizinho(i)->get_tipo() != OBSTACULO)
+            {
                 partida->get_vizinho(i)->set_g(partida->get_g() + (i % 2 == 0 ? (i % 4 == 0 ? peso_v : peso_h) : peso_d));
                 partida->get_vizinho(i)->set_p_pai(partida);
-                busca_profundidade(partida->get_vizinho(i), chegada);
-            }
-        }
-
-        for (int i = 0; i < 8; i++){
-            if (partida->get_vizinho(i) != nullptr){
+                busca_profundidade(partida->get_vizinho(i), chegada, peso_v, peso_h, peso_d);
                 partida->get_vizinho(i)->set_p_pai(nullptr);
+                partida->get_vizinho(i)->set_g(0);
             }
         }
     }
