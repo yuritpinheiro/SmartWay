c24a211125a54b4483d7908b8ae6e4f3a78bbc38
Yuri Thomas <yuri.tpinheiro@gmail.com>
2015-09-17 23:09:32 -0300

    Implementação do compare

--- algoritmos_busca.h
+++ algoritmos_busca.h
@@ -6,18 +6,20 @@
 
 /* Algoritmo A* */
 
-using namespace std;
-
-struct avaliar_caminho
+void calcular_heuristica(Celula ** mapa, int altura, int largura, Celula* chegada)
 {
-    bool operator()(const Celula& a,const Celula& b) const
+    for (int i = 0; i < altura; i++)
     {
-        return a.f < b.f;
+        for (int j = 0; j < largura; j++)
+        {
+            if (mapa[i][j].get_tipo() != PARTIDA || mapa[i][j].get_tipo() != CHEGADA)
+                mapa[i][j].set_h(abs(chegada->get_x() - i) + abs(chegada->get_y() - j));
+        }
     }
-};
+}
 
 void a_estrela(Celula *partida, double h, double v, double d){
-    priority_queue<Celula, vector<Celula>, avaliar_caminho> fila;
+    std::priority_queue<Celula*, std::vector<Celula*>, avaliar_custo> fila;
 
     Celula *aux = partida;
     double custo_horizontal = h; //recebo da interface
@@ -25,40 +27,35 @@ void a_estrela(Celula *partida, double h, double v, double d){
     double custo_diagonal = d;
     double g = 0, f;
 
-
     aux->set_g(0);
     while(aux->get_tipo()!= CHEGADA){
 
         for(int i = 0; i < 8; i++){
             if( aux->get_vizinho(i) != nullptr && aux->get_vizinho(i)->get_tipo() == LIVRE){
 
-                    if(i == 1 || i == 5){
-                        g = custo_vertical + aux->get_g();
-                    }else if(i == 3 || i == 7){
-                        g = custo_horizontal + aux->get_g();;
-                    }else{
-                        g = custo_diagonal + aux->get_g();;
-                    }
+                if(i == 1 || i == 5){
+                    g = custo_vertical + aux->get_g();
+                }else if(i == 3 || i == 7){
+                    g = custo_horizontal + aux->get_g();;
+                }else{
+                    g = custo_diagonal + aux->get_g();;
+                }
 
-                    f = g + aux->get_vizinho(i)->get_h();
+                f = g + aux->get_vizinho(i)->get_h();
 
-                    if(aux->get_vizinho(i)->get_pai()== nullptr || (aux->get_vizinho(i)->get_pai()!= nullptr && aux->get_vizinho(i)->get_g() < g)){
-                        aux->get_vizinho(i)->set_pai(aux);
-                        aux->get_vizinho(i)->set_g(g);
-                        aux->get_vizinho(i)->set_f(f);
-                        //fila.push(aux->get_vizinho(i));
-                    }
+                if(aux->get_vizinho(i)->get_pai()== nullptr || (aux->get_vizinho(i)->get_pai()!= nullptr && aux->get_vizinho(i)->get_g() < g)){
+                    aux->get_vizinho(i)->set_pai(aux);
+                    aux->get_vizinho(i)->set_g(g);
+                    aux->get_vizinho(i)->set_f(f);
+                    //fila.push(aux->get_vizinho(i));
+                }
             }
         }
 
         //aux = fila.top();
         fila.pop();
     }
-
-        //priotity_queue.pop();
-
 }
-        //teste dos irmãos ??
 
 /* --------------------- */
 
